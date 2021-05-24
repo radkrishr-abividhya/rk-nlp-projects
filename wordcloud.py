82a08237d6eefa811461b8ebc3d73ecbbf0e4970
Radhakrishnan Rajagopalan <radha@abividhya.com>
2021-05-25 00:58:25 +0530

    added wordcloud.py and spam.csv

---
+++ wordcloud.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# pip install wordcloud
+from wordcloud import WordCloud
+
+
+# data from:https://www.kaggle.com/uciml/sms-spam-collection-dataset
+df = pd.read_csv('spam.csv', encoding='ISO-8859-1')
+
+# drop unnecessary columns
+df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1)
+
+# rename columns to something better
+df.columns = ['labels', 'data']
+
+# visualize the data
+def visualize(label):
+  words = ''
+  for msg in df[df['labels'] == label]['data']:
+    msg = msg.lower()
+    words += msg + ' '
+  wordcloud = WordCloud(width=600, height=400).generate(words)
+  plt.imshow(wordcloud)
+  plt.axis('off')
+  plt.show()
+
+visualize('spam')
